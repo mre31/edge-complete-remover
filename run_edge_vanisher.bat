31eaf519b167e8cbf8424b263b7bcfa6f5d3f355
NovaWeb <74352257+mre31@users.noreply.github.com>
2025-01-17 03:45:49 +0300

    Delete run_edge_vanisher.bat

--- run_edge_vanisher.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0edge_vanisher.ps1""' -Verb RunAs"
-exit 
